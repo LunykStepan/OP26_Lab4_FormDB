2c1a3991dc2994dfced13dae9e03c49d3f8bd079
LunykStepan <lunyk.stepan@chnu.edu.ua>
2026-02-12 20:55:44 +0200

    Add files via upload

--- OP26_Lab4_FormDB/MainForm.cpp
+++ OP26_Lab4_FormDB/MainForm.cpp
@@ -1,2 +1,14 @@
 #include "MainForm.h"
 
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+int main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Lab4_Furniture::MainForm form;
+	Application::Run(% form);
+	return 0;
+}
--- OP26_Lab4_FormDB/MainForm.h
+++ OP26_Lab4_FormDB/MainForm.h
@@ -1,6 +1,6 @@
 #pragma once
 
-namespace OP26Lab4FormDB {
+namespace Lab4_Furniture {
 
 	using namespace System;
 	using namespace System::ComponentModel;
@@ -8,25 +8,16 @@ namespace OP26Lab4FormDB {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
-
-	/// <summary>
-	/// Summary for MainForm
-	/// </summary>
+	using namespace System::Data::SqlClient; 
 	public ref class MainForm : public System::Windows::Forms::Form
 	{
 	public:
 		MainForm(void)
 		{
 			InitializeComponent();
-			//
-			//TODO: Add the constructor code here
-			//
 		}
 
 	protected:
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
 		~MainForm()
 		{
 			if (components)
@@ -34,49 +25,185 @@ namespace OP26Lab4FormDB {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Label^ label1;
-	protected:
 
 	private:
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		System::ComponentModel::Container ^components;
+		System::Windows::Forms::Label^ lblName;
+		System::Windows::Forms::Label^ lblType;
+		System::Windows::Forms::Label^ lblBrand;
+		System::Windows::Forms::Label^ lblManufacturer;
+		System::Windows::Forms::Label^ lblSupplier;
+		System::Windows::Forms::Label^ lblPrice;
+
+		System::Windows::Forms::TextBox^ txtName;
+		System::Windows::Forms::TextBox^ txtType;
+		System::Windows::Forms::TextBox^ txtBrand;
+		System::Windows::Forms::TextBox^ txtManufacturer;
+		System::Windows::Forms::TextBox^ txtSupplier;
+		System::Windows::Forms::TextBox^ txtPrice;
+
+		System::Windows::Forms::Button^ btnAdd;
+
+		System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->lblName = (gcnew System::Windows::Forms::Label());
+			this->lblType = (gcnew System::Windows::Forms::Label());
+			this->lblBrand = (gcnew System::Windows::Forms::Label());
+			this->lblManufacturer = (gcnew System::Windows::Forms::Label());
+			this->lblSupplier = (gcnew System::Windows::Forms::Label());
+			this->lblPrice = (gcnew System::Windows::Forms::Label());
+			this->txtName = (gcnew System::Windows::Forms::TextBox());
+			this->txtType = (gcnew System::Windows::Forms::TextBox());
+			this->txtBrand = (gcnew System::Windows::Forms::TextBox());
+			this->txtManufacturer = (gcnew System::Windows::Forms::TextBox());
+			this->txtSupplier = (gcnew System::Windows::Forms::TextBox());
+			this->txtPrice = (gcnew System::Windows::Forms::TextBox());
+			this->btnAdd = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
-			// 
-			// label1
-			// 
-			this->label1->AutoSize = true;
-			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 11, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(204)));
-			this->label1->ForeColor = System::Drawing::Color::DarkBlue;
-			this->label1->Location = System::Drawing::Point(131, 17);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(131, 24);
-			this->label1->TabIndex = 0;
-			this->label1->Text = L"Lab4_FormDB";
-			// 
-			// MainForm
-			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->lblName->AutoSize = true;
+			this->lblName->Location = System::Drawing::Point(20, 23);
+			this->lblName->Name = L"lblName";
+			this->lblName->Size = System::Drawing::Size(39, 13);
+			this->lblName->TabIndex = 0;
+			this->lblName->Text = L"Назва";
+			this->lblType->AutoSize = true;
+			this->lblType->Location = System::Drawing::Point(20, 53);
+			this->lblType->Name = L"lblType";
+			this->lblType->Size = System::Drawing::Size(26, 13);
+			this->lblType->TabIndex = 1;
+			this->lblType->Text = L"Вид";
+			this->lblBrand->AutoSize = true;
+			this->lblBrand->Location = System::Drawing::Point(20, 83);
+			this->lblBrand->Name = L"lblBrand";
+			this->lblBrand->Size = System::Drawing::Size(40, 13);
+			this->lblBrand->TabIndex = 2;
+			this->lblBrand->Text = L"Марка";
+			this->lblManufacturer->AutoSize = true;
+			this->lblManufacturer->Location = System::Drawing::Point(20, 113);
+			this->lblManufacturer->Name = L"lblManufacturer";
+			this->lblManufacturer->Size = System::Drawing::Size(56, 13);
+			this->lblManufacturer->TabIndex = 3;
+			this->lblManufacturer->Text = L"Виробник";
+			this->lblSupplier->AutoSize = true;
+			this->lblSupplier->Location = System::Drawing::Point(20, 143);
+			this->lblSupplier->Name = L"lblSupplier";
+			this->lblSupplier->Size = System::Drawing::Size(79, 13);
+			this->lblSupplier->TabIndex = 4;
+			this->lblSupplier->Text = L"Постачальник";
+			this->lblPrice->AutoSize = true;
+			this->lblPrice->Location = System::Drawing::Point(20, 173);
+			this->lblPrice->Name = L"lblPrice";
+			this->lblPrice->Size = System::Drawing::Size(29, 13);
+			this->lblPrice->TabIndex = 5;
+			this->lblPrice->Text = L"Ціна";
+			this->txtName->Location = System::Drawing::Point(140, 20);
+			this->txtName->Name = L"txtName";
+			this->txtName->Size = System::Drawing::Size(180, 20);
+			this->txtName->TabIndex = 6;
+			this->txtType->Location = System::Drawing::Point(140, 50);
+			this->txtType->Name = L"txtType";
+			this->txtType->Size = System::Drawing::Size(180, 20);
+			this->txtType->TabIndex = 7;
+			this->txtBrand->Location = System::Drawing::Point(140, 80);
+			this->txtBrand->Name = L"txtBrand";
+			this->txtBrand->Size = System::Drawing::Size(180, 20);
+			this->txtBrand->TabIndex = 8;
+			this->txtManufacturer->Location = System::Drawing::Point(140, 110);
+			this->txtManufacturer->Name = L"txtManufacturer";
+			this->txtManufacturer->Size = System::Drawing::Size(180, 20);
+			this->txtManufacturer->TabIndex = 9;
+			this->txtSupplier->Location = System::Drawing::Point(140, 140);
+			this->txtSupplier->Name = L"txtSupplier";
+			this->txtSupplier->Size = System::Drawing::Size(180, 20);
+			this->txtSupplier->TabIndex = 10;
+			this->txtPrice->Location = System::Drawing::Point(140, 170);
+			this->txtPrice->Name = L"txtPrice";
+			this->txtPrice->Size = System::Drawing::Size(180, 20);
+			this->txtPrice->TabIndex = 11;
+			this->btnAdd->Location = System::Drawing::Point(140, 210);
+			this->btnAdd->Name = L"btnAdd";
+			this->btnAdd->Size = System::Drawing::Size(100, 30);
+			this->btnAdd->TabIndex = 12;
+			this->btnAdd->Text = L"Додати";
+			this->btnAdd->UseVisualStyleBackColor = true;
+			this->btnAdd->Click += gcnew System::EventHandler(this, &MainForm::btnAdd_Click);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(542, 311);
-			this->Controls->Add(this->label1);
+			this->ClientSize = System::Drawing::Size(350, 270);
+			this->Controls->Add(this->btnAdd);
+			this->Controls->Add(this->txtPrice);
+			this->Controls->Add(this->txtSupplier);
+			this->Controls->Add(this->txtManufacturer);
+			this->Controls->Add(this->txtBrand);
+			this->Controls->Add(this->txtType);
+			this->Controls->Add(this->txtName);
+			this->Controls->Add(this->lblPrice);
+			this->Controls->Add(this->lblSupplier);
+			this->Controls->Add(this->lblManufacturer);
+			this->Controls->Add(this->lblBrand);
+			this->Controls->Add(this->lblType);
+			this->Controls->Add(this->lblName);
 			this->Name = L"MainForm";
-			this->Text = L"MainForm";
+			this->Text = L"Меблі (Варіант 12)";
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
+
+	private:
+		System::Void btnAdd_Click(System::Object^ sender, System::EventArgs^ e) {
+
+			if (txtName->Text == "" || txtType->Text == "" || txtBrand->Text == "" ||
+				txtManufacturer->Text == "" || txtSupplier->Text == "" || txtPrice->Text == "")
+			{
+				MessageBox::Show("Будь ласка, заповніть усі поля!", "Помилка", MessageBoxButtons::OK, MessageBoxIcon::Warning);
+				return;
+			}
+
+			try {
+				String^ connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=FurnitureDB;Integrated Security=True";
+				SqlConnection^ con = gcnew SqlConnection(connectionString);
+
+				con->Open();
+
+				String^ query = "INSERT INTO Furniture (Name, Type, Brand, Manufacturer, Supplier, Price) " +
+					"VALUES (@nm, @tp, @br, @mn, @sp, @pr)";
+
+				SqlCommand^ cmd = gcnew SqlCommand(query, con);
+
+				cmd->Parameters->AddWithValue("@nm", txtName->Text);
+				cmd->Parameters->AddWithValue("@tp", txtType->Text);
+				cmd->Parameters->AddWithValue("@br", txtBrand->Text);
+				cmd->Parameters->AddWithValue("@mn", txtManufacturer->Text);
+				cmd->Parameters->AddWithValue("@sp", txtSupplier->Text);
+
+				try {
+					cmd->Parameters->AddWithValue("@pr", Convert::ToDecimal(txtPrice->Text));
+				}
+				catch (Exception^) {
+					MessageBox::Show("Ціна має бути числом (наприклад: 1200,50)", "Помилка формату");
+					con->Close();
+					return;
+				}
+
+				cmd->ExecuteNonQuery();
+				con->Close();
+
+				MessageBox::Show("Запис успішно додано!", "Успіх", MessageBoxButtons::OK, MessageBoxIcon::Information);
+
+				txtName->Clear();
+				txtType->Clear();
+				txtBrand->Clear();
+				txtManufacturer->Clear();
+				txtSupplier->Clear();
+				txtPrice->Clear();
+			}
+			catch (Exception^ ex) {
+				MessageBox::Show("Помилка бази даних:\n" + ex->Message, "Помилка", MessageBoxButtons::OK, MessageBoxIcon::Error);
+			}
+		}
 	};
 }
